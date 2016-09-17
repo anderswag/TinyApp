c108549612164e0ada717f8c3b1280d082a87f11
Anderson Hung <anderson.hung@live.com>
2016-09-17 16:29:24 +0000

    Added error handling

--- index.js
+++ index.js
@@ -2,25 +2,36 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const methodOverride = require('method-override');
+//MONGODB INFO ------------
+const MongoClient = require("mongodb").MongoClient;
+const urlDb = require('./modules/database');
 
 let app = express();
 const PORT = process.env.PORT || 8080;
 
-var urlDatabase = {
+
+//database(MONGODB_URI);
+
+/*var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-};
+};*/
 
 //GENERATE
 
 function generateRandomString() {
-  var char = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for(var i = 0; i < 7; i++){
+  let char = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(let i = 0; i < 7; i++){
     char += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return char;
 }
+
+//Get long URL
+
+
+
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded());
@@ -30,15 +41,18 @@ app.set("view engine", "ejs");
 //Routes --------------------------------------------Routes
 app.delete("/urls/:id", (req, res) => {
   let deleteKey = req.params.id;
-  delete urlDatabase[deleteKey];
+  urlDb.deleteURL(deleteKey)
+  //delete urlDatabase[deleteKey];
+
   res.redirect('/urls/');
-})
+});
 
 app.put("/urls/:id", (req, res) => {
-  let newURL = req.body.newURL
-  urlDatabase[req.params.id] = newURL;
+  let newURL = req.body.newURL;
+  //urlDatabase[req.params.id] = newURL;
+  urlDb.updateURL(req.params.id, newURL);
   res.redirect('/urls/');
-})
+});
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -48,12 +62,19 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
+
+
 app.get("/urls", (req, res) => {
-  let templateVars = {
-    urls: urlDatabase
-    };
-  res.render("urls_index", templateVars);
+  urlDb.getDB((err, results) =>{
+      console.log('stuff' + results);
+      res.render("urls_index", {all:results});
+    });
 });
+
+/*urlDb.test();*/
+
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -61,24 +82,62 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString()
-  urlDatabase[randomString] = req.body.longURL;
+  //urlDatabase[randomString] = req.body.longURL;
+  urlDb.createURL(randomString, req.body.longURL);
   console.log(randomString);
-  res.redirect(`/urls/${randomString}`);
+  res.redirect(`/urls`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  let shortURL = req.params.shortURL;
+  console.log(shortURL);
+  urlDb.getLongURL(shortURL, (err, longURL) =>{
+      console.log(err);
+      if(err){
+        res.send(err.message || 'Do I even exist bro? I think therefore I am.');
+      } else {
+        //res.render("urls_show", {long : longURL, shortURL: shortURL});
+        res.redirect(longURL);
+      }
+    });
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-  res.render("urls_show", templateVars);
+  let shortURL = req.params.id;
+  console.log(shortURL);
+  urlDb.getLongURL(shortURL, (err, longURL) =>{
+      console.log(err);
+      if(err){
+        res.send(err.message || 'Do I even exist bro? I think therefore I am.');
+      } else {
+        res.render("urls_show", {long : longURL, shortURL: shortURL});
+      }
+    });
 });
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
+//ERROR HANDLING
+app.get('*', function(req, res, next) {
+  var err = new Error();
+  err.status = 404;
+  next(err);
+});
+
+// handling 404 errors
+app.use(function(err, req, res, next) {
+  if(err.status !== 404) {
+    return next();
+  }
+
+  res.send(err.message || 'Do I even exist bro? I think therefore I am.');
+});
+
+
+
 
 
 // LISTENING ----------------------------------------------
---
+++ modules/database.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
+
+function getLongURL(shortURL, cb) {
+  MongoClient.connect(MONGODB_URI, (err, db) => {
+
+    db.collection("urls").findOne({ shortURL }, (err, result) => {
+/*      if (err){
+        cb(err);
+        return;
+      }*/
+      console.log(result);
+      if(result != null){
+        cb(null, result.longURL);
+      } else {
+        let error = "Can't find it";
+        cb(error);
+      }
+    });
+
+  });
+}
+
+function getDB(cb){
+  MongoClient.connect(MONGODB_URI, (err, db) => {
+
+    db.collection("urls").find().toArray((err, results) => {
+      if (err) {
+        cb(err);
+        return;
+      }
+      cb(null ,results);
+      return;
+    });
+
+  });
+}
+
+function deleteURL(deleteKey){
+  MongoClient.connect(MONGODB_URI, (err, db) => {
+    db.collection("urls").deleteOne({shortURL:deleteKey});
+  });
+}
+
+function createURL(short, long) {
+  MongoClient.connect(MONGODB_URI, (err, db) => {
+    db.collection("urls").insertOne({shortURL:short, longURL:long});
+  });
+}
+
+function updateURL(short, long) {
+  MongoClient.connect(MONGODB_URI, (err, db) => {
+    db.collection("urls").updateOne({shortURL:short},{shortURL:short, longURL:long});
+  });
+}
+
+module.exports =  {getLongURL: getLongURL, getDB: getDB, deleteURL:deleteURL, createURL:createURL, updateURL:updateURL};
